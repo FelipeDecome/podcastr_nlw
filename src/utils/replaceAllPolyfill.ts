f26e90db954e4a0948ded8e896f50a778f725f3d
Felipe Decome <decome.felipe@gmail.com>
2022-06-08 14:45:17 -0300

    chore: removing unnecessary code

--- src/utils/replaceAllPolyfill.ts
+++
@@ -1,7 +0,0 @@
-if (!String.prototype.replaceAll) {
-    String.prototype.replaceAll = function replaceAll(search, replace) {
-        return this.split(search).join(replace);
-    };
-}
-
-export {};
